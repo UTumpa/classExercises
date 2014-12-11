c13ced52f58961e0ba56d125e78e95a7fb467347
tanjina <tanjina_tushar@yahoo.com>
2014-12-11 18:08:26 +0600

    create3_function

--- Arrayfunction/array2function.php
+++ Arrayfunction/array2function.php
@@ -1,25 +1,21 @@
 <?php
 
-// improving the previous script
-function ack_r3(&$array, $case=CASE_LOWER, $flag_rec=false)
-{
-    $array = array_change_key_case($array, $case);
-    if ( $flag_rec ) {
-        foreach ($array as $key => $value) {
-            if ( is_array($value) ) {
-                ack_r3($array[$key], $case, true);
-            }
+function arrKey2Lower(&$arrVals) {
+    foreach( $arrVals as $key => $item ) {
+        $key2 = strtolower($key);
+        if ( $key2 != $key) {
+            unset($arrVals[$key]);
+            $arrVals[$key2]=$item;
+            $key=$key2;
         }
+
+        if ( is_array($item) ) { arrKey2Lower($arrValores[$key]); }
     }
 }
 
-// same of array_change_key_case
 $arr = array('ID' => 1, 'NAME'=> 'Luis', 'Contact' => array('PHONE' => '3010-7148', 'E-MAIL' => 'luis@net.com') );
-ack_r3($arr, CASE_LOWER,false);
+arrKey2Lower($arr);
 var_dump($arr);
 
-$arr = array('ID' => 1, 'NAME'=> 'Luis', 'Contact' => array('PHONE' => '3010-7148', 'E-MAIL' => 'luis@net.com') );
-ack_r3($arr, CASE_LOWER,true);
-var_dump($arr);
 
 ?>
